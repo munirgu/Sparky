4ec39cb194de477ec0c26d138e405d4a11b8b024
munirgu <munirgu@maestralsolutions.com>
2021-09-14 15:15:36 +0200

    Basic tests

---
+++ SparkyNUnitTest/CalculatorNUnitTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using Sparky;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkyNUnitTest
+{
+    [TestFixture]
+    public class CalculatorNUnitTests
+    {
+        [Test]
+
+        public void AddNumbers_InputTwoInt_GetCorrectAddition()
+        {
+            //arrange
+
+            Calculator calc = new Calculator();
+
+            //act
+
+            int result = calc.AddNumbers(10, 20);
+
+            //assert
+
+            Assert.AreEqual(30, result);
+        }
+
+
+        [Test]
+        public void IsOddNumber_InputInt_GetCorrectBoolean()
+        {
+            //arrange
+
+            Calculator calc = new Calculator();
+
+            //act
+
+            bool result = calc.IsOddNumber(3);
+
+            //assert
+
+            Assert.AreEqual(true,result);
+        }
+
+    }
+}
